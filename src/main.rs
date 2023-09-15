37403739c1d0097c6bc979bebeaaacbd6fe5ec37
Pholawat Tangsatit <pholawat.tangsatit@hotmail.com>
2023-09-15 16:51:05 +0700

    minor refactor & style changes

--- src/main.rs
+++ src/main.rs
@@ -24,78 +24,86 @@ fn main() -> Result<()> {
         .build()
         .unwrap();
 
-    let enums: Vec<Enum> = pool.install(|| {
-        let projects_iter = config.gitlab_projects.into_par_iter();
-        let enums = projects_iter.flat_map(|project| -> Vec<Enum> {
-            let tree_endpoint = Tree::builder()
-                .project(project.name.clone())
-                .ref_(project.branch.clone())
-                .recursive(true)
-                .build();
-
-            let tree_endpoint = match tree_endpoint {
-                Ok(tree_endpoint) => tree_endpoint,
-                Err(_e) => return vec![],
-            };
-
-            let trees = api::paged(tree_endpoint, api::Pagination::All).query(&gitlab_client);
-
-            let trees: Vec<RepoTreeObject> = match trees {
-                Ok(trees) => trees,
-                Err(_e) => return vec![],
-            };
-
-            let trees_iter = trees.into_par_iter();
-            let enums_in_project = trees_iter.flat_map(|tree| {
-                let mut enums_in_file: Vec<Enum> = vec![];
-
-                if tree.mode != "100644" {
-                    return enums_in_file;
-                };
-
-                let file_extension = tree.name.split(".").last();
-                let file_endpoint = FileRaw::builder()
+    let all_enums: Vec<Enum> = pool.install(|| {
+        config
+            .gitlab_projects
+            .into_par_iter()
+            .flat_map(|project| {
+                let tree_endpoint = Tree::builder()
                     .project(project.name.clone())
                     .ref_(project.branch.clone())
-                    .file_path(tree.path)
+                    .recursive(true)
                     .build();
 
-                let file_endpoint = match file_endpoint {
-                    Ok(file_endpoint) => file_endpoint,
-                    Err(_e) => return enums_in_file,
+                let tree_endpoint = match tree_endpoint {
+                    Ok(tree_endpoint) => tree_endpoint,
+                    Err(_e) => return vec![],
                 };
 
-                let file_content = api::raw(file_endpoint).query(&gitlab_client);
+                let git_trees =
+                    api::paged(tree_endpoint, api::Pagination::All).query(&gitlab_client);
 
-                let file_content = match file_content {
-                    Ok(file_content) => file_content,
-                    Err(_e) => return enums_in_file,
+                let git_trees: Vec<RepoTreeObject> = match git_trees {
+                    Ok(trees) => trees,
+                    Err(_e) => return vec![],
                 };
 
-                let file_content = match String::from_utf8(file_content) {
-                    Ok(file_content) => file_content,
-                    Err(_e) => return enums_in_file,
-                };
+                let enum_in_current_repo = git_trees
+                    .into_par_iter()
+                    .flat_map(|git_tree| {
+                        let mut enums_in_current_file: Vec<Enum> = vec![];
 
-                match file_extension {
-                    Some("scala") => {
-                        println!("Executing Scala Parsers: {}", tree.name);
-                        enums_in_file.extend(EnumerationParser::parse_enums(&file_content));
-                        enums_in_file.extend(CaseObjectParser::parse_enums(&file_content));
-                    }
-                    _ => (),
-                }
+                        if git_tree.mode != "100644" {
+                            return enums_in_current_file;
+                        };
 
-                enums_in_file
-            });
+                        let file_extension = git_tree.name.split(".").last();
+                        let file_endpoint = FileRaw::builder()
+                            .project(project.name.clone())
+                            .ref_(project.branch.clone())
+                            .file_path(git_tree.path)
+                            .build();
 
-            enums_in_project.collect()
-        });
+                        let file_endpoint = match file_endpoint {
+                            Ok(file_endpoint) => file_endpoint,
+                            Err(_e) => return enums_in_current_file,
+                        };
 
-        enums.collect()
+                        let file_content = api::raw(file_endpoint).query(&gitlab_client);
+
+                        let file_content = match file_content {
+                            Ok(file_content) => file_content,
+                            Err(_e) => return enums_in_current_file,
+                        };
+
+                        let file_content = match String::from_utf8(file_content) {
+                            Ok(file_content) => file_content,
+                            Err(_e) => return enums_in_current_file,
+                        };
+
+                        match file_extension {
+                            Some("scala") => {
+                                println!("Executing Scala Parsers: {}", git_tree.name);
+                                enums_in_current_file
+                                    .extend(EnumerationParser::parse_enums(&file_content));
+                                enums_in_current_file
+                                    .extend(CaseObjectParser::parse_enums(&file_content));
+                            }
+                            _ => {
+                                println!("No parser for file: {}", git_tree.name);
+                            }
+                        }
+
+                        enums_in_current_file
+                    })
+                    .collect();
+
+                enum_in_current_repo
+            })
+            .collect()
     });
 
-    println!("Enums: {:#?}", enums);
+    println!("Enums: {:#?}", all_enums);
 
     Ok(())
 }
